c5014a61bff68c084563a9b3924a1826e7d506b5
Prasanth Dasararaju <prasanthd46@gmail.com>
2025-12-07 14:23:37 +0530

    chore:Add file

---
+++ Backend/api/[...path].ts
@@ -0,0 +1,9 @@
+import serverless from 'serverless-http';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import app from '../src/index';
+
+const handler = serverless(app);
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  return handler(req as any, res as any);
+};
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
 import cookieParser from 'cookie-parser';
+import serverless from "serverless-http";
 
 dotenv.config();
 
@@ -18,7 +19,4 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
